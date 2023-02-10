16e2f07fd339282718053d506452ccd4dd3be8ac
Myself-Ikram <97652328+Myself-Ikram@users.noreply.github.com>
2023-02-10 12:53:08 +0530

    update projects.jsx

--- src/components/resume/ResumeDesc/Projects.jsx
+++ src/components/resume/ResumeDesc/Projects.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
-import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Project from "./Project";
 export default function Projects() {
   return (
